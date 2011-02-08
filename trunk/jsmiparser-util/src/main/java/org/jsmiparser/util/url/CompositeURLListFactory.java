942121fd830152e23202b9ca91e4ec8048294b19
jialun.liu@gmail.com <jialun.liu@gmail.com@72596c6f-fa99-5695-859b-d47b7f96127b>
2011-02-08 07:28:16 +0000

    add unit test for url

--- trunk/jsmiparser-util/src/main/java/org/jsmiparser/util/url/CompositeURLListFactory.java
+++ trunk/jsmiparser-util/src/main/java/org/jsmiparser/util/url/CompositeURLListFactory.java
@@ -21,7 +21,6 @@ import java.util.Arrays;
 import java.util.List;
 
 public class CompositeURLListFactory implements URLListFactory {
-
 	private List<URLListFactory> children;
 
 	public CompositeURLListFactory() {
